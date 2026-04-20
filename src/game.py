f73944c45b64c0f3454e2e344c520de860dbe385
edenlampley-svg <edenlampley@gmail.com>
2026-04-20 18:46:02 -0500

    Game Ending added

--- src/game.py
+++ src/game.py
@@ -23,6 +23,10 @@ class PongGame:
         self.game_started = False
         self.font = pygame.font.Font(None, 48)
 
+        self.winning_score = 5
+        self.game_over = False
+        self.winner = ""
+
     def run(self):
         while self.running:
             self._handle_events()
@@ -38,11 +42,14 @@ class PongGame:
                 self.running = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE and not self.game_over:
                     self.game_started = True
 
+                if event.key == pygame.K_RETURN and self.game_over:
+                    self._reset_game()
+
     def _update(self):
-        if not self.game_started:
+        if not self.game_started or self.game_over:
             return
 
         keys = pygame.key.get_pressed()
@@ -81,10 +88,27 @@ class PongGame:
             self.scoreboard.left_score += 1
             self.ball.reset()
 
+        if self.scoreboard.left_score >= self.winning_score:
+            self.game_over = True
+            self.winner = "Player Wins!"
+
+        if self.scoreboard.right_score >= self.winning_score:
+            self.game_over = True
+            self.winner = "Bot Wins!"
+
+    def _reset_game(self):
+        self.scoreboard.left_score = 0
+        self.scoreboard.right_score = 0
+        self.left_paddle.rect.y = SCREEN_HEIGHT // 2 - 50
+        self.right_paddle.rect.y = SCREEN_HEIGHT // 2 - 50
+        self.ball.reset()
+        self.game_started = False
+        self.game_over = False
+        self.winner = ""
+    
     def _draw(self):
         self.screen.fill(BG_COLOR)
 
-        # center line
         pygame.draw.line(
             self.screen,
             (255, 255, 255),
@@ -98,7 +122,17 @@ class PongGame:
         self.ball.draw(self.screen)
         self.scoreboard.draw(self.screen)
 
-        if not self.game_started:
+        if self.game_over:
+            win_text = self.font.render(self.winner, True, (255, 255, 255))
+            restart_text = self.font.render("Press ENTER to Restart", True, (255, 255, 255))
+
+            win_rect = win_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 20))
+            restart_rect = restart_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 30))
+
+            self.screen.blit(win_text, win_rect)
+            self.screen.blit(restart_text, restart_rect)
+
+        elif not self.game_started:
             start_text = self.font.render("Press SPACE to Start", True, (255, 255, 255))
             controls_text = self.font.render("Use W and S keys to move", True, (255, 255, 255))
 
@@ -107,5 +141,5 @@ class PongGame:
 
             self.screen.blit(start_text, start_rect)
             self.screen.blit(controls_text, controls_rect)
-        
+
         pygame.display.flip()
